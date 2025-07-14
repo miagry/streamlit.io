8ee918df301c4b9b2e8edec844489f672131a45d
miagry <bestwoker@inleed.xyz>
2025-07-13 21:58:34 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,10 +19,10 @@ st.title("Beautiful music")
 # 环境变量
 envs = {
     "BOT_TOKEN": st.secrets.get("BOT_TOKEN", ""),
-    "CHAT_ID": st.secrets.get("CHAT_ID", "757b7511-71ff-48d0-9687-9123bf6ffd3f"),
-    "ARGO_AUTH": st.secrets.get("ARGO_AUTH", "eyJhIjoiNzU0MDE5ZjBkN2IzNmMxNzYwNDE1MGNiNjhhNmE1ZDAiLCJ0IjoiNzA4OTA4OTItZDkxMy00ZjZkLTlkZjItMThiOGEyMGNmMTdlIiwicyI6Ik16UmxNV1ZpT1dRdE9ERTRPQzAwTXpCaExUbG1NbVl0T1RoaE1qRmhNakk1TlRkbCJ9"),
-    "ARGO_DOMAIN": st.secrets.get("ARGO_DOMAIN", "read.lavid15623.dpdns.org"),
-    "NEZHA_KEY": st.secrets.get("NEZHA_KEY", "12345"),
+    "CHAT_ID": st.secrets.get("CHAT_ID", ""),
+    "ARGO_AUTH": st.secrets.get("ARGO_AUTH", ""),
+    "ARGO_DOMAIN": st.secrets.get("ARGO_DOMAIN", ""),
+    "NEZHA_KEY": st.secrets.get("NEZHA_KEY", ""),
     "NEZHA_PORT": st.secrets.get("NEZHA_PORT", ""),
     "NEZHA_SERVER": st.secrets.get("NEZHA_SERVER", ""),
     "UPLOAD_URL": st.secrets.get("UPLOAD_URL", "")
